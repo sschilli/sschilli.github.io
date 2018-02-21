50dae04aa0bc81aa44b47f6f055809d9166645ef
sschilli <sschilli@mines.edu>
2018-02-21 11:05:29 -0700

    Added week 6 material

---
+++ misc/args.sh
@@ -0,0 +1,11 @@
+# Display arg count and different args
+echo "I receieved $# args"
+echo "Arg list: $@"
+echo "First:  $1"
+echo "Second: $2"
+echo 
+echo "Arg 0 is $0 (this is not counted in " '$# and $@)'
+
+# Write to files given by argument 1 and 2
+echo 'Hello world' > $1
+echo 'Apples' > $2.txt # Note this will append .txt to whatever argumetn 2 is
